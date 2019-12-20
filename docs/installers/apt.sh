588f8ae2b1e0e3ac081ff3973f579badaa325351
Peter <pfeerick@users.noreply.github.com>
2019-12-20 12:43:39 +1000

    Removed unnecessary semi-colons
    Extra characters, can add to confusion as not consistently used in file.

--- docs/installers/apt.sh
+++ docs/installers/apt.sh
@@ -6,22 +6,22 @@ if [ "$(id -u)" != "0" ]; then
 	exit 1
 fi
 
-MACHINE_MTYPE="$(uname -m)";
-ARCH="${MACHINE_MTYPE}";
-REPO_VENDOR="headmelted";
+MACHINE_MTYPE="$(uname -m)"
+ARCH="${MACHINE_MTYPE}"
+REPO_VENDOR="headmelted"
 DEPENDENCIES=""
 
-if [ "$ARCH" = "amd64" ] || [ "$ARCH" = "i386" ]; then REPO_VENDOR="microsoft"; fi;
+if [ "$ARCH" = "amd64" ] || [ "$ARCH" = "i386" ]; then REPO_VENDOR="microsoft"; fi
 
-echo "Architecture detected as $ARCH...";
+echo "Architecture detected as $ARCH..."
 
 echo "Updating APT cache..."
 if apt-get -qq update; then
-  echo "Update complete.";
+  echo "Update complete."
 else
-  echo "Update failed.";
-  exit 1;
-fi;
+  echo "Update failed."
+  exit 1
+fi
 
 #test what dependencies need to be installed
 test -f "/usr/share/doc/apt-transport-https/copyright" || DEPENDENCIES="${DEPENDENCIES} apt-transport-https"
@@ -36,50 +36,50 @@ if [ "X${DEPENDENCIES}" != "X" ]; then
 fi
 
 if [ "${REPO_VENDOR}" = "headmelted" ]; then
-  gpg_key=https://packagecloud.io/headmelted/codebuilds/gpgkey;
-  repo_name="stretch";
-  repo_entry="deb https://packagecloud.io/headmelted/codebuilds/debian/ ${repo_name} main";
-  code_executable_name="code-oss";
+  gpg_key=https://packagecloud.io/headmelted/codebuilds/gpgkey
+  repo_name="stretch"
+  repo_entry="deb https://packagecloud.io/headmelted/codebuilds/debian/ ${repo_name} main"
+  code_executable_name="code-oss"
 else
-  gpg_key=https://packages.microsoft.com/keys/microsoft.asc;
+  gpg_key=https://packages.microsoft.com/keys/microsoft.asc
   repo_name="stable"
-  repo_entry="deb https://packages.microsoft.com/repos/vscode ${repo_name} main";
-  code_executable_name="code-insiders";
-fi;
+  repo_entry="deb https://packages.microsoft.com/repos/vscode ${repo_name} main"
+  code_executable_name="code-insiders"
+fi
 
-echo "Retrieving GPG key [${REPO_VENDOR}] ($gpg_key)...";
+echo "Retrieving GPG key [${REPO_VENDOR}] ($gpg_key)..."
 curl -fsSL $gpg_key | sudo apt-key add -
 
-echo "Removing any previous entry to headmelted repository";
-rm -rf /etc/apt/sources.list.d/headmelted_codebuilds.list;
-rm -rf /etc/apt/sources.list.d/codebuilds.list;
+echo "Removing any previous entry to headmelted repository"
+rm -rf /etc/apt/sources.list.d/headmelted_codebuilds.list
+rm -rf /etc/apt/sources.list.d/codebuilds.list
   
-echo "Installing [${REPO_VENDOR}] repository...";
-echo "${repo_entry}" > /etc/apt/sources.list.d/${REPO_VENDOR}_vscode.list;
+echo "Installing [${REPO_VENDOR}] repository..."
+echo "${repo_entry}" > /etc/apt/sources.list.d/${REPO_VENDOR}_vscode.list
   
 echo "Refreshing APT cache again..."
 if apt-get -qq update; then
-  echo "Repository install complete.";
+  echo "Repository install complete."
 else
-  echo "Repository install failed.";
-  exit 1;
-fi;
+  echo "Repository install failed."
+  exit 1
+fi
 
-echo "Installing Visual Studio Code from [${repo_name}] (this may take several minutes)...";
+echo "Installing Visual Studio Code from [${repo_name}] (this may take several minutes)..."
 if apt-get install -t ${repo_name} -y -qq ${code_executable_name}; then
-  echo "Visual Studio Code install complete.";
+  echo "Visual Studio Code install complete."
 else
-  echo "Visual Studio Code install failed.";
-  exit 1;
-fi;
+  echo "Visual Studio Code install failed."
+  exit 1
+fi
 
-echo "Installing any dependencies that may have been missed...";
+echo "Installing any dependencies that may have been missed..."
 if apt-get install -y -qq -f; then
-  echo "Missed dependency install complete.";
+  echo "Missed dependency install complete."
 else
-  echo "Missed dependency install failed.";
-  exit 1;
-fi;
+  echo "Missed dependency install failed."
+  exit 1
+fi
 
 echo "
 
@@ -89,4 +89,4 @@ You can start code at any time by calling \"${code_executable_name}\" within a t
 
 A shortcut should also now be available in your desktop menus (depending on your distribution).
 
-";
+"
